85fd0a89cfed680439a13c45d70adb5d0e5986ce
Anand Desai <desaiaj@rose-hulman.edu>
2024-01-10 03:34:25 -0500

    refactor: minor edit to day 7 part 1 enum

--- day7/soln.go
+++ day7/soln.go
@@ -67,13 +67,13 @@ var cardRankMap = map[rune]int{
 }
 
 var cardHandTypeMap = map[HandType]int{
-	"FIVE_OF_A_KIND":  7,
-	"FOUR_OF_A_KIND":  6,
-	"FULL_HOUSE":      5,
-	"THREE_OF_A_KIND": 4,
-	"TWO_PAIR":        3,
-	"ONE_PAIR":        2,
-	"HIGH_CARD":       1,
+	FIVE_OF_A_KIND:  7,
+	FOUR_OF_A_KIND:  6,
+	FULL_HOUSE:      5,
+	THREE_OF_A_KIND: 4,
+	TWO_PAIR:        3,
+	ONE_PAIR:        2,
+	HIGH_CARD:       1,
 }
 
 type CardHand struct {
